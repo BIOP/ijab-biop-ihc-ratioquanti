236c16fa7cd2e34da4797b5e12fc96e4ffce9ddf
Romain Guiet <guiet.romain@gmail.com>
2018-01-24 09:42:14 +0100

    update the update.jar file with correct name

--- updateJar.bat
+++ updateJar.bat
@@ -1,12 +1,12 @@
 @ECHO OFF
 
-ECHO Copying Biop_VSI_reader.ijm to current folder
+ECHO Copying BIOP_IHC_RatioQuanti.ijm to current folder
 copy C:\Fiji\plugins\ActionBar\Debug\BIOP_IHC_RatioQuanti.ijm %~dp0
 
 ECHO Creating JAR FILE
 jar cf BIOP_IHC_RatioQuanti.jar plugins.config icons *.ijm
 
-ECHO Copying Biop_VSI_reader.jar to Fiji folder
+ECHO Copying BIOP_IHC_RatioQuanti.ijm to Fiji folder
 copy *.jar C:\Fiji\plugins\BIOP
 
 PAUSE
